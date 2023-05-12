8361d0997ec5c55e2b7eeef3a13375edaab26bcf
GFXTI <65578807+GFXTI@users.noreply.github.com>
2023-05-12 15:36:13 -0700

    get function for file system

---
+++ SchoolProjs/get.lua
@@ -0,0 +1,28 @@
+local dirs = {}
+local funcs = {}
+
+for i,v in listfiles"Progen" do
+    table.insert(dirs, v:sub(8))
+end
+
+function funcs:Get(directory)
+    local getfuncs = {}
+    local files = listfiles `"Progen"/{directory}`
+    if not dirs[directory] then return error `{directory} is not a valid directory` end
+
+    function getfuncs:Load(file, ...)
+        return loadfile(files[`Progen/{directory}/{file}`])(...)
+    end
+
+    function getfuncs:Read(file)
+        return readfile(files[`Progen/{directory}/{file}`])
+    end
+
+    function getfuncs:Write(file, contents)
+        local err, suc = pcall(writefile, files[`Progen/{directory}/{file}`], contents)
+
+        if not suc then return error(err) end
+    end
+
+    return getfuncs
+end
