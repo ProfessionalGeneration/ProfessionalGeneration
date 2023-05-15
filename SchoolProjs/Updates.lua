82a0ba5cbfa70ec0465bbac1c40b457423d10dd1
GFXTI <65578807+GFXTI@users.noreply.github.com>
2023-05-15 01:13:34 -0700

    Update Updates.lua

--- SchoolProjs/Updates.lua
+++ SchoolProjs/Updates.lua
@@ -1,5 +1,8 @@
+local Get = loadfile("Progen/libraries/Get.lua")()
+local Services = Get:Get"libraries":Load "Services"
+
 local directories = {
-    "libs",
+    "libraries",
     "games",
     "base"
 }
@@ -7,6 +10,14 @@ local directories = {
 for _, dir in directories do
     for __, file in Get:Get(dir):List() do
         local filehash = syn.crypt.custom.hash("md5", readfile(file))
-        local gottenhash = syn.request {}.Body
+        local filename = file:split("/")[3]
+        local gitresponse = Services.Http:JSONDecode(syn.request{Url = `https://api.github.com/repos/GFXTI/ProfessionalGeneration/branches/main/{dir}/{filename}`}.Body)
+        local hash = gitresponse.commit.sha
+
+        if hash ~= filehash then
+            local writedata = syn.request{`https://raw.githubusercontent.com/GFXTI/ProfessionalGeneration/main/{dir}/{filename}.lua`}.Body
+
+            writefile(file, writedata)
+        end
     end
 end
