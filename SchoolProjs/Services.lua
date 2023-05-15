ba5f917df9c7372fd1a760df6ed488ab03a283ad
GFXTI <65578807+GFXTI@users.noreply.github.com>
2023-05-15 01:14:09 -0700

    Create Services.lua

---
+++ SchoolProjs/Services.lua
@@ -0,0 +1,12 @@
+local Known = {
+    Tween = cloneref(game:service"TweenService"),
+    Core = cloneref(game:service"CoreGui"),
+    Http = cloneref(game:service "HttpService"),
+    Run = cloneref(game:service "RunService"),
+    Players = cloneref(game:service "Players"),
+    Teams = cloneref(game:service "Teams"),
+    Storage = cloneref(game:service "ReplicatedStorage"),
+    First = cloneref(game:service "ReplicatedFirst")
+}
+
+return Known
