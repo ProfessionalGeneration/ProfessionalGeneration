a2db662b4566cdac71f4aa3ab1f631e580f86774
GFXTI <65578807+GFXTI@users.noreply.github.com>
2023-05-19 12:46:24 -0700

    real or fake

--- libraries/Notifications.lua
+++ libraries/Notifications.lua
@@ -1,4 +1,5 @@
 local Notes = {}
+local current = {}
 -- being so deadass rn snuffles on twitter so fucking CUTE
 
 local function Show(box, outlines, size)
@@ -13,6 +14,24 @@ local function Show(box, outlines, size)
         Box.Size = Vector2.new(Math.Lerp(20, finalsize, inc), 20)
         Box.Position = Vector2.new(Math.Lerp(10, 50, inc), Box.Position.Y)
         outlines:Update()
+    end)
+end
+
+local function Hide(box)
+    table.remove(current, table.find(current, box))
+    local old = {}
+    for i,v in current do
+        old[v] = v.Position.Y
+    end
+
+    task.spawn(Math.DeltaIter, 0, 1, 20, function(inc)
+        inc = Easing.Out.Quad(inc)
+
+        for i,v in current do
+            if v.__object.Position.Y > box.__object.Position.Y then
+                v.Position = Vector2.new(v.Position.X, Math.Lerp(old[v], old[v] - 25, inc))
+            end
+        end
     end)
 end
 
@@ -31,7 +50,7 @@ end
 do
     local finalsize = GetTextSize(settings.Text, 16, Drawing.Fonts["Monospace"]).X + 8
     local Box = Objects.Frame {
-        Position = Vector2.new(10, 40 + (25 * (#Notes - 3))),
+        Position = Vector2.new(10, 40 + (25 * (#current - 1))),
         Size = Vector2.new(20, 20),
         Color = Color3.new(.15, .15, .15),
         Opacity = 0,
@@ -42,7 +61,8 @@ do
         Size = 15,
         Color = Color3.new(1,1,1),
         Outlined = true,
-        Text = settings.Text
+        Text = settings.Text,
+        Opacity = 0
     }
     local Outlines = Objects.Outline(Box)
     Show(Box, Outlines, Size)
