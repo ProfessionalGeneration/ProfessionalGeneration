dad956475f7ce0cc97c447db373444125ded3495
GFXTI <65578807+GFXTI@users.noreply.github.com>
2023-05-26 00:22:53 -0700

    formatted

--- libraries/Notifications.lua
+++ libraries/Notifications.lua
@@ -1,18 +1,19 @@
-local Notes = {}
-local current = {}
+local Get, File, Directory = loadfile("Progen/libraries/FileSystem.lua")()
+local Objects, Math, Easing, Services = Get:Get"libraries":Get"DrawObjects":Load(), Get:Get"libraries":Get"Math":Load(), Get:Get"libraries":Get"Easing":Load(), Get:Get"libraries":Get"Services":Load()
+local Notes, current = {}, {}
 
 local function Show(box, outlines, size)
     table.insert(box, current)
     Math.DeltaIter(0, 1, 60, function(inc)
         inc = Easing.Out.Quad(inc)
 
-        for i,v in Box:Children(true) do
+        for i,v in box:Children(true) do
             v.Opacity = inc
         end
 
-        Box.Opacity = inc
-        Box.Size = Vector2.new(Math.Lerp(20, size, inc), 20)
-        Box.Position = Vector2.new(Math.Lerp(10, 50, inc), Box.Position.Y)
+        box.Opacity = inc
+        box.Size = Vector2.new(Math.Lerp(20, size, inc), 20)
+        box.Position = Vector2.new(Math.Lerp(10, 50, inc), box.Position.Y)
         outlines:Update()
     end)
 end
@@ -24,7 +25,7 @@ local function GetPosXByOptions(boxes)
         pos += v.Size.X
     end
 
-    return pos
+    return pos + (#boxes * 5)
 end
 
 local function GetNoteY()
@@ -33,6 +34,8 @@ local function GetNoteY()
     for i,v in current do
         pos += v.Size.Y
     end
+
+    return pos + (#current * 5)
 end
 
 local function Hide(box)
@@ -51,13 +54,33 @@ local function Hide(box)
 
         for i,v in current do
             if v.__object.Position.Y > box.__object.Position.Y then
-                v.Position = Vector2.new(v.Position.X, Math.Lerp(old[v] - 25, old[v] inc))
+                v.Position = Vector2.new(v.Position.X, Math.Lerp(old[v] - 25, old[v], inc))
             end
         end
     end)
 end
 
-function Notes:Note(settings)
+local function GetDefaultNote(settings)
+    local Box = Objects.Frame {
+        Position = Vector2.new(10, 40 + GetNoteY()),
+        Size = Vector2.new(20, 20),
+        Color = Color3.new(.15, .15, .15),
+        Opacity = 0,
+    }
+    local Text = Objects.Text {
+        Position = Vector2.new(2, 2),
+        Parent = Box,
+        Size = 15,
+        Color = Color3.new(1,1,1),
+        Outlined = true,
+        Text = settings.Text
+    }
+    local Outlines = Objects.Outline(Box)
+
+    return Box, Text, Outlines
+end
+
+function Notes:Notify(settings)
     local sets = {
         Time = 5,
         Error = false,
@@ -67,34 +90,20 @@ function Notes:Note(settings)
     for i,v in settings do
         sets[i] = v
     end
-end
 
-do
     local finalsize = Math.GetTextSize(settings.Text, 16, Drawing.Fonts["Monospace"]).X + 8
-    local Box = Objects.Frame {
-        Position = Vector2.new(10, 40 + GetNoteY()),
-        Size = Vector2.new(23, 23),
-        Color = Color3.new(.15, .15, .15),
-        Opacity = 0,
-    }
-    local Text = Objects.Label {
-        Position = Vector2.new(2, 2),
-        Parent = Box,
-        Size = 16,
-        Color = Color3.new(1,1,1),
-        Outlined = true,
-        Text = settings.Text,
-        Opacity = 0
-    }
+    local Box, Text, Outlines = GetDefaultNote(settings)
+
     local Timer = Objects.Line {
-        From = Vector2.zero,
-        To = Vector2.zero,
+        From = Vector2.new(0, Box.Size.Y - 2),
+        To = Vector2.new(0, Box.Size.Y - 2),
         Parent = Box,
         Opacity = 0,
-        Thickness = 2        
+        Thickness = 2
     }
-    local Outlines = Objects.Outline(Box)
     local stop
+
+    Show(Box, Outlines, finalsize)
 
     local function StartTimer()
         local i = 0
@@ -102,7 +111,7 @@ do
         while not stop do
             if i >= sets.Time then break end
             i += Services.Run.RenderStepped:wait()
-            timer.To = Vector2.new(Math.Lerp(0, box.Size, i), 23)
+            Timer.To = Vector2.new(Math.Lerp(0, Box.Size.Y - 2, i), 23)
         end
 
         if not stop then Hide(Box) end
@@ -120,34 +129,31 @@ do
     end)
 end
 
-do
+function Notes:Question(settings)
+    local sets = {
+        Text = "Note",
+        Options = {
+            "Yes",
+            "No"
+        }
+    }
+
+    for i,v in settings do
+        sets[i] = v
+    end
+
     local finalsize = Math.GetTextSize(settings.Text, 16, Drawing.Fonts["Monospace"]).X + 8
+    local Box, Text, Outlines = GetDefaultNote(settings)
     local answered = Instance.new "BindableEvent"
     local options = {}
-    local Box = Objects.Frame {
-        Position = Vector2.new(10, 40 + GetNoteY()),
-        Size = Vector2.new(20, 20),
-        Color = Color3.new(.15, .15, .15),
-        Opacity = 0,
-    }
-    local Text = Objects.Label {
-        Position = Vector2.new(2, 2),
-        Parent = Box,
-        Size = 16,
-        Color = Color3.new(1,1,1),
-        Outlined = true,
-        Text = settings.Text,
-        Opacity = 0
-    }
-    local Outlines = Objects.Outline(Box)
-    
+
     for i,v in settings.Options do
         local textsize = Math.GetTextSize(v, 14, Drawing.Fonts["Monospace"]).X + 2
-            
+
         local box = Objects.Frame {
-            Position = Vector2.new(GetPosXByOptions(options) + (#options * 5), 30),
+            Position = Vector2.new(GetPosXByOptions(options), 30),
             Parent = Box,
-            Size = Vector2.new(textsize),
+            Size = Vector2.new(textsize, 12),
             Color = Color3.new(.15, .15, .15),
             Opacity = 0,
             Active = true
@@ -157,27 +163,43 @@ do
             Position = Vector2.one,
             Size = 14,
             Parent = box,
-            Size = Vector2.new(textsize, 12),
             Text = v,
             Opacity = 0,
             Outlined = true,
             Color = Color3.new(1,1,1),
         }
+        local outlime = Objects.Outline(text)
+        box.Changed:Connect(outlime.Update)
 
-        box.Button1Down:Connect(function()
-            Answered:Fire(v)
+        box.Button1Down:Once(function()
+            answered:Fire(v)
         end)
+
+        options[v] = box
     end
 
-    Show(Box, Outlines, Size)
+    Show(Box, Outlines, finalsize)
 
-    Answered.Event:Once(function()
+    answered.Event:Once(function()
         Hide(Box)
     end)
 
     return {
-        ["Answered"] = Answered.Event,
+        ["Answered"] = answered.Event,
     }
+end
+
+function Notes:Yield(settings)
+    local sets = {
+        Text = "Note",
+    }
+
+    for i,v in settings do
+        sets[i] = v
+    end
+
+    local finalsize = Math.GetTextSize(settings.Text, 16, Drawing.Fonts["Monospace"]).X + 8
+    local Box = GetDefaultNote(settings)
 end
 
 return Notes
