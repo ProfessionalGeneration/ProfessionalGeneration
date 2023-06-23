f36dd3c8807d8c566203f2f55c4627346ac97e81
GFXTI <65578807+GFXTI@users.noreply.github.com>
2023-06-23 00:03:09 -0700

    Update Updates.lua

--- libraries/Updates.lua
+++ libraries/Updates.lua
@@ -1,5 +1,5 @@
-local Get = loadfile("Progen/libraries/Get.lua")()
-local Services = Get:Get"libraries":Get"Services":Load()
+local Get = isfile("Progen/libraries/FileSystem.lua") and loadfile("Progen/libraries/FileSystem.lua")()
+local Http = cloneref(game:service"HttpService")
 
 local directories = {
     "libraries",
@@ -7,12 +7,22 @@ local directories = {
     "base"
 }
 
+if not Get then
+    makefolder "Progen"
+
+    for i,v in directories do
+        makefolder(`Progen/{v}`)
+    end
+
+    Get = loadstring(syn.request {Url = "https://raw.githubusercontent.com/GFXTI/ProfessionalGeneration/main/libraries/FileSystem.lua"})()
+end
+
 for _, dir in directories do
     local directory = Get:Get(dir)
 
-    for _, file in Services.Http:JSONDecode(syn.request{Url = `https://api.github.com/repos/GFXTI/ProfessionalGeneration/git/trees/{dir}/recursive=1`}.Body).tree do
+    for _, file in Http:JSONDecode(syn.request{Url = `https://api.github.com/repos/GFXTI/ProfessionalGeneration/git/trees/{dir}/recursive=1`}.Body).tree do
         if not direcotry:Get(file.path) then
-            directory:File(file.path, syn.request{`https://raw.githubusercontent.com/GFXTI/ProfessionalGeneration/main/{dir}/{file.path}`}.Body)
+            directory:File(file.path, syn.request{Url = `https://raw.githubusercontent.com/GFXTI/ProfessionalGeneration/main/{dir}/{file.path}`}.Body)
         end
     end
 end
@@ -21,11 +31,11 @@ for _, dir in directories do
     for __, file in Get:Get(dir):List() do
         local filehash = syn.crypt.custom.hash("md5", readfile(file))
         local filename = file:split("/")[3]
-        local gitresponse = Services.Http:JSONDecode(syn.request{Url = `https://api.github.com/repos/GFXTI/ProfessionalGeneration/branches/main/{dir}/{filename}`}.Body)
+        local gitresponse = Http:JSONDecode(syn.request{Url = `https://api.github.com/repos/GFXTI/ProfessionalGeneration/branches/main/{dir}/{filename}`}.Body)
         local hash = gitresponse.commit.sha
 
         if hash ~= filehash then
-            writefile(file, syn.request{`https://raw.githubusercontent.com/GFXTI/ProfessionalGeneration/main/{dir}/{filename}.lua`}.Body)
+            writefile(file, syn.request{Url = `https://raw.githubusercontent.com/GFXTI/ProfessionalGeneration/main/{dir}/{filename}.lua`}.Body)
         end
     end
 end
